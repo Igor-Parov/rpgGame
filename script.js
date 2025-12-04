ad699d72ed047b9899da5186e0dd7d0f87947d0a
Igor-Parov <ihor207415@student-schoolsofwestfield.org>
2025-12-04 16:59:01 +0000

    123

---
+++ script.js
@@ -0,0 +1,39 @@
+let health = 100;
+let xp = 0;
+let gold = 50;
+let inventory = ["stick"];
+let fighting;
+let currentWeapon = 0;
+let monsterHealth;
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterNameText = document.querySelector("#monsterNameText");
+const monsterHealthText = document.querySelector("#monsterHealthText");
+
+
+// initialize buttons
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+
+function goStore(){
+console.log("Going to store")
+
+}
+function goCave(){
+
+
+}
+function fightDragon(){
+
+
+}
